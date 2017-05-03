c2c3305b207bb2cc2b69b930e6f2b78f0b267711
Jeffrey Reinecke <jeff@paploo.net>
2017-05-02 22:26:24 -0700

    Refactored Expression to be type-safe.

--- src/main/scala/net/paploo/diestats/expression/ast/Expression.scala
+++ src/main/scala/net/paploo/diestats/expression/ast/Expression.scala
@@ -4,33 +4,25 @@ import net.paploo.diestats.expression.ast.Expression.{Convolve, Plus, Values}
 
 import scala.language.higherKinds
 
-trait Evaluator[A, R] {
-  def fromValues(as: Seq[A]): R
-  def convolve(x: R, y: R): R
-}
-
-trait NumericEvaluator[A, R] extends Evaluator[A, R] {
-  def plus(x: R, y: R): R
-}
-
-object Evaluator {
-
-  trait StringEvaluator extends Evaluator[String, String] {
-    override def fromValues(as: Seq[String]): String = as.toString
-    override def convolve(x: String, y: String): String = s"$x convolve $y"
-  }
-  object StringEvaluator extends StringEvaluator
-
-  object IntNumericEvaluator extends NumericEvaluator[Int, Int] {
-    override def fromValues(as: Seq[Int]): Int = as.head
-
-    override def convolve(x: Int, y: Int): Int = x+y
-
-    override def plus(x: Int, y: Int): Int = convolve(x,y)
-  }
-
-}
-
+/**
+  * Expression encapsulates a node in the AST over a domain A.
+  *
+  * Expressions may be evaluated with a variety of Evaluator
+  * implementations, which may do different things. For example,
+  * one may give a human readable string form of the expression,
+  * while another evaluates with a random number generator, and
+  * yet another evaluates as a ProbabilityDistribution[A].
+  *
+  * The evaluator type is specified using a higher-kinded type
+  * to facilitate bounds imposed by the various concrete expression
+  * nodes. For example, Convolve should work with any evaluator, whereas
+  * Minus may only be evaluated by a numerical evaluator. Making the
+  * evaluator be a bounded type parameter ensures that composition
+  * of the nodes retains the most restrictive evaluator.
+  *
+  * @tparam A
+  * @tparam E
+  */
 trait Expression[A, -E[X,Y] <: Evaluator[X,Y]] {
   def apply[R](e: E[A,R]): R
 }
@@ -56,27 +48,30 @@ object Runner {
   def main(args: Array[String]): Unit = {
 
     val foo: Expression[String, Evaluator] = Convolve(Values("foo", "bar"), Values("alpha", "beta"))
-    val resultFoo: String = foo.apply(Evaluator.StringEvaluator)
+    val resultFoo: String = foo.apply(Evaluator.Stringifier)
     println(resultFoo)
 
     val bar: Expression[Int, NumericEvaluator] = Plus(Values(1,2), Values(3,4))
-    val resultBar: Int = bar.apply(Evaluator.IntNumericEvaluator)
+    val resultBar: Int = bar.apply(Evaluator.RandomNumericReflexive)
     println(resultBar)
 
     //Shouldn't compile because plus requires NumericDomainEvaluator.
     //val bar2: Expression[Int, Evaluator] = Plus(Values(1,2), Values(3,4))
 
+    //We can be over-sepcific too:
+    val fooN: Expression[String, NumericEvaluator] = foo
+
 
     val cfoo: Expression[String, Evaluator] = Convolve(foo, foo)
+    println(cfoo(Evaluator.Stringifier))
 
     //We don't want this to work, since bar requires NumericDomainEvaluator.
     //val cbar: Expression[Int, Evaluator] = Convolve(bar, bar)
 
     val cbar2: Expression[Int, NumericEvaluator] = Convolve(bar, bar)
-    println(cbar2(Evaluator.IntNumericEvaluator))
+    println(cbar2(Evaluator.RandomNumericReflexive))
 
   }
 
 }
-
 
