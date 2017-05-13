081866e2b9c873dfa8396ce0e08dd81c7e85f517
Jeffrey Reinecke <jeff@paploo.net>
2017-05-12 19:04:37 -0700

    Implemented the final round of tess

--- src/test/scala/net/paploo/diestats/expression/ast/ExpressionTest.scala
+++ src/test/scala/net/paploo/diestats/expression/ast/ExpressionTest.scala
@@ -209,16 +209,21 @@ class ExpressionTest extends SpecTest {
 
   describe("Int Domain Expression") {
 
-    val random = new java.util.Random {
-      // For testing, always return the expectation value.
-      override def nextInt(bound: Int): Int = (bound - 1) / 2
+    def constRandom(n: Int) = new java.util.Random {
+      override def nextInt(bound: Int): Int = {
+        assert(bound > 0, "bound must be positive")
+        assert(n >= 0 && n < bound, s"Cannot legally return value $n on call nextInt($bound)")
+        n
+      }
     }
 
     def stringEvaluator() = DiceExpressionStringEvaluator[Int]
 
-    def orderedDirectEvaluator() = DirectEvaluator.ordered[Int](random)
+    def orderedDirectEvaluator(random: java.util.Random) = DirectEvaluator.ordered[Int](random)
 
-    def numericDirectEvaluator() = DirectEvaluator.numeric[Int](random)
+    def numericDirectEvaluator(random: java.util.Random) = DirectEvaluator.numeric[Int](random)
+
+    def probabilityEvaluator() = ProbabilityDistributionEvaluator.numeric[Int]
 
     describe("values") {
 
@@ -230,15 +235,27 @@ class ExpressionTest extends SpecTest {
       }
 
       it("should evaluate to a value") {
-        val resultL = d6.apply(orderedDirectEvaluator())
-        resultL should === (3)
+        (0 until 6).foreach { i =>
+          val orderedResult = d6.apply(orderedDirectEvaluator(constRandom(i)))
+          orderedResult should === (i+1) //rand 0-5 produces sides 1-6
+        }
 
-        val resultR = d6.apply(numericDirectEvaluator())
-        resultR should === (3)
+        (0 until 6).foreach { i =>
+          val numericResult = d6.apply(numericDirectEvaluator(constRandom(i)))
+          numericResult should === (i+1) //rand 0-5 produces sides 1-6
+        }
       }
 
       it("should have the right probability distribution domain") {
-        pending
+        val result = d6(probabilityEvaluator())
+        result.pairs should === (Seq(
+          1 -> Probability(1,6),
+          2 -> Probability(1,6),
+          3 -> Probability(1,6),
+          4 -> Probability(1,6),
+          5 -> Probability(1,6),
+          6 -> Probability(1,6)
+        ))
       }
 
     }
@@ -257,19 +274,33 @@ class ExpressionTest extends SpecTest {
       }
 
       it("should evaluate to a value") {
-        val resultR = d6.apply(numericDirectEvaluator())
-        resultR should === (3)
+        val result = d6.apply(numericDirectEvaluator(constRandom(2)))
+        result should === (3) // rand 0 produces side 1, rand 1 produces side 2, rand 2 produces side 3.
+      }
+
+      it("should evaluate to a value for d3") {
+        //This is useful for making sure the "best" test is supplied good data!
+        val d3 = Expression.DieValue(3)
+        d3(numericDirectEvaluator(constRandom(1))) should === (2)
       }
 
       it("should have the right probability distribution domain") {
-        pending
+        val result = d6(probabilityEvaluator())
+        result.pairs should === (Seq(
+          1 -> Probability(1,6),
+          2 -> Probability(1,6),
+          3 -> Probability(1,6),
+          4 -> Probability(1,6),
+          5 -> Probability(1,6),
+          6 -> Probability(1,6)
+        ))
       }
 
     }
 
     describe("convolve") {
       val d6 = Expression.DieValue(6)
-      val convolveExpr = Expression.Convolve(d6, d6)
+      val convolveExpr = Expression.Convolve(d6, d6) //Equivalent to 2d6.
 
       it("should evaluate to string") {
         val result = convolveExpr.apply(stringEvaluator())
@@ -281,24 +312,38 @@ class ExpressionTest extends SpecTest {
       }
 
       it("should evaluate to a value") {
-        val resultR = convolveExpr.apply(numericDirectEvaluator())
-        resultR should === (6) // 3 + 3, since evaluates each independently.
+        val result = convolveExpr.apply(numericDirectEvaluator(constRandom(2)))
+        result should === (6) // 3 + 3, since evaluates each independently.
       }
 
       it("should have the right probability distribution domain") {
-        pending
+        val result = convolveExpr(probabilityEvaluator())
+        //The classic 2d6 distribution.
+        result.pairs should === (Seq(
+          2  -> Probability(1,36),
+          3  -> Probability(2,36),
+          4  -> Probability(3,36),
+          5  -> Probability(4,36),
+          6  -> Probability(5,36),
+          7  -> Probability(6,36),
+          8  -> Probability(5,36),
+          9  -> Probability(4,36),
+          10 -> Probability(3,36),
+          11 -> Probability(2,36),
+          12 -> Probability(1,36)
+        ))
       }
 
     }
 
     describe("best") {
 
-      val d6 = Expression.DieValue(6)
-      val bestExpr = Expression.Best(3, Seq(d6, d6, d6, d6))
+      val d3 = Expression.DieValue(3)
+      val bestExpr = Expression.Best(3, Seq(d3, d3, d3, d3))
 
       it("should evaluate to string") {
         val result = bestExpr.apply(stringEvaluator())
-        result should === ("(List(d6, d6, d6, d6) b 3)")
+        result should === ("(List(d3, d3, d3, d3) b 3)")
       }
 
       it("should not compile with an ordered evaluator (needs numeric)") {
@@ -306,12 +351,21 @@ class ExpressionTest extends SpecTest {
       }
 
       it("should evaluate to a value") {
-        val resultR = bestExpr.apply(numericDirectEvaluator())
-        resultR should === (9) //It selects 3, 3, 3 and sums.
+        val resultR = bestExpr.apply(numericDirectEvaluator(constRandom(0)))
+        resultR should === (3) //The "random" generator selects 1,1,1 as the best of 1,1,1,1 and sums.
       }
 
       it("should have the right probability distribution domain") {
-        pending
+        // This one is tricky to derive by hand, since 4 d3s make 81 outcomes!
+        // However, it's easy to write a quick script to give you the probabilities:
+        val d3 = Seq(1,2,3)
+        val outcomes = for { x <- d3; y <- d3; z <- d3; u <- d3 } yield Seq(x,y,z,u).sorted.takeRight(3).sum
+        val countPairs = outcomes.groupBy(identity).mapValues(_.length).toSeq.sortBy(_._1)
+        // countPairs =  Vector((3,1), (4,4), (5,10), (6,17), (7,22), (8,18), (9,9)), btw.
+        val expectedPairs = countPairs.map(pair => pair._1 -> Probability(pair._2, 81))
+
+        val result = bestExpr(probabilityEvaluator())
+        result.pairs should === (expectedPairs)
       }
 
     }
@@ -341,12 +395,26 @@ class ExpressionTest extends SpecTest {
       }
 
       it("should evaluate to a value") {
-        val resultR = stmtExpr.apply(numericDirectEvaluator())
-        resultR should === (6) // 3 + 3
+        val result = stmtExpr.apply(numericDirectEvaluator(constRandom(2)))
+        result should === (6) // 3 + 3
       }
 
       it("should have the right probability distribution domain") {
-        pending
+        val result = stmtExpr(probabilityEvaluator())
+        //The classic 2d6 distribution.
+        result.pairs should === (Seq(
+          2  -> Probability(1,36),
+          3  -> Probability(2,36),
+          4  -> Probability(3,36),
+          5  -> Probability(4,36),
+          6  -> Probability(5,36),
+          7  -> Probability(6,36),
+          8  -> Probability(5,36),
+          9  -> Probability(4,36),
+          10 -> Probability(3,36),
+          11 -> Probability(2,36),
+          12 -> Probability(1,36)
+        ))
       }
 
     }
